e03a11029621392ff226d0bd3bd411884b23af70
itsamanverma <amanvermame786@gmail.com>
2020-06-11 20:49:23 +0530

    admin user & register api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email')->unique();
+            $table->string('firstname'); 
+            $table->string('lastname');
+            $table->date('dob')->nullable();
+            $table->string('region')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password'); 
+            $table->rememberToken();
+            $table->timestamps();
+            $table->string('verifytoken')->nullable();
+        });
+
+        // Create the initial admin user
+        DB::table('users')->insert([
+            'email' => 'admin@admin.com',
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'password' => bcrypt('password'),
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
