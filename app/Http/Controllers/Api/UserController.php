596adf8f3f6e601ec1549c719f405271a9a594ac
itsamanverma <amanvermame786@gmail.com>
2020-06-11 23:55:28 +0530

    register api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -13,6 +13,33 @@ class UserController extends Controller
     /**
      * function to register user
      * 
-     * @
+     *@param Request $request
+     *@return Illuminate\Http\Response 
      */
+     public function register(Request $request){
+
+        $input = $request->all();
+
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required|max:20',
+            'lastname' => 'required|max:20',
+            'email' => 'bail|required|email|unique:users',
+            'password' => 'required|min:8|max:15',
+            'dob' => 'required|date_format:Y-M-D|before:today',
+            'region' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 210);
+        }
+
+        $input["created_at"] = now();
+        $input['password'] = bcrypt($input['password']);
+        $input['verifytoken'] = str_random(60);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('fundoo')->accessToken;
+        $success['firstname'] = $user->firstname;
+        // event(new UserRegistered($user, $input['verifytoken']));
+        return response()->json(['message' => 'registration succesfull'], 201);
+     }
 }
