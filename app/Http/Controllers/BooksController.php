aff379f58634850f5fda1cd75cdac6793019acee
itsamanverma <amanvermame786@gmail.com>
2020-06-16 13:24:27 +0530

    get books list api

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -27,4 +27,18 @@ class BooksController extends Controller
         $book = Books::createBooks($data);
         return response()->json(['message' => 'Book added', 'id' =>$book->id], 201);
     }
+
+    /**
+     * create the function to get listofBooks
+     * 
+     * @return Illuminate\Http\Response
+     */
+    public function getListBook() {
+
+        Cache::forget('books' . Auth::user()->id);
+        $books = Cache::remember('books' . Auth::user()->id, (30), function () {
+            $bb = Books::with('books')->where('user_id', Auth::user()->id)->get();
+            return $bb;
+        });
+    }
 }
